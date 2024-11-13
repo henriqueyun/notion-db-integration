54567faed7e552f3766a6c07280a387368a41ef8
henriqueyun <henriqueyun@gmail.com>
2024-11-12 22:39:10 -0300

    docs: added swagger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import app from './app'
 
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yaml')
+const fs = require("fs")
+const file  = fs.readFileSync('./swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const server = app.listen(process.env.PORT || 8080, () => {
 	console.log(`Server listening at ${process.env.PORT || 8080}`)
 })
